24a0209ce9c94e4c19699bb7fe297c197e0aa626
haruxkn <s231036@kashiwanoha.ed.jp>
2025-06-25 12:18:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function changeText() {
-  document.getElementById("greeting").textContent = "やったね！文字が変わったよ！";
+function takePhoto() {
+  const shutterSound = document.getElementById('shutterSound');
+  shutterSound.currentTime = 0; // 最初から再生できるようにリセット
+  shutterSound.play();
+
+  const canvas = document.createElement('canvas');
+  canvas.width = videoElement.videoWidth;
+  canvas.height = videoElement.videoHeight;
+
+  const context = canvas.getContext('2d');
+  context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+  const dataURL = canvas.toDataURL('image/png');
+  console.log("写真撮影成功:", dataURL);
 }
